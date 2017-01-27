0f9b69ba2e1e09419350ea120fb141c5123a2f62
Michael Regan <michael.regan141@gmail.com>
2017-01-27 00:31:56 -0700

    Unique variables determined, using regex for string search

---
+++ calc2chains.py
@@ -0,0 +1,58 @@
+# converting predicate calculus in spreadsheet to be causal chains
+# Jan 2017
+
+
+import csv, re
+
+
+path = '/Users/Michael/Desktop/FD-v0118.csv'
+
+myDict={}
+
+calcDict={}
+
+with open(path, 'r', encoding = "ISO-8859-1") as g:
+    g.readline()
+    reader = csv.reader(g, delimiter=',')
+    
+    count=1
+    for row in reader:
+        vn, asc, fd1, fd2, fd3, example, sem_plain, sem_variables, notes = row
+        myDict[count]=row
+        count+=1
+        
+for v in myDict.values():
+    if v[7] not in calcDict:
+        calcDict[v[7]]=[]
+        
+
+
+def searchVariables(string):
+    #identifying string between parenthesis
+    
+    strVar=re.search(r'\((.*?)\)',string).group(1).split(',')
+    
+    return strVar
+    
+    
+    
+    
+preamble=r"\documentclass{article} \usepackage{tikz-dependency} \usepackage{mathtools} \usepackage{tikz} \usepackage{caption} \usepackage{amsmath} \usepackage{stackengine} \begin{document}".split()
+
+for k, v in calcDict.items():
+    thisList=[]
+    variables=[]
+    for i in k.split('&'):
+        thisList.append(i.strip())
+    for j in thisList:
+        var=searchVariables(j)
+        for v in var:
+            if v not in variables:
+                variables.append(v)
+    print(thisList, variables)
+    print()            
+    
+        
+        
+    
+       
